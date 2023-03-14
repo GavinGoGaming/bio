4287232039c1b177fd71fe04b8095fbdae390bbe
GavinGoGaming Fox <gavingogaming@gmail.com>
2023-03-14 02:49:17 +0000

    h

--- httpProxy.js
+++ httpProxy.js
@@ -3,12 +3,20 @@ var proxys = require('http-proxy');
 var app = express();
 var proxy = proxys.createProxyServer({
     changeOrigin: true,
-    ssl: false
-})
+    secure: false,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    },
+    xfwd: true,
+});  
 
-app.use('/', (req, res) => {
-    proxy.web(req, res, { target: 'https://win11.blueedge.me' });
-})
+app.use(require('body-parser').json());
+app.use(express.static(__dirname));
+
+app.use('/proxy', (req, res) => {
+    console.log(req.body);
+    proxy.web(req, res, { target: req.body.site });
+});
 
 app.listen(3000, ()=>{
     console.log('l');
